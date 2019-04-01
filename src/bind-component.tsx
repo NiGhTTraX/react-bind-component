b7a368dc59090e8defd4f6c1a4219e1452c32a76
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-04-01 14:50:22 +0200

    Mark comment as TODO

--- src/bind-component.tsx
+++ src/bind-component.tsx
@@ -2,6 +2,7 @@
 import React, { Component, ComponentType } from 'react';
 
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+
 /**
  * Create a HOC that binds some props for the wrapped component.
  */
@@ -13,9 +14,9 @@ export default function bindComponent<T, K extends keyof T>(
     static displayName = `bind(${C.displayName || C.name})`;
 
     render() {
+      // TODO: https://github.com/Microsoft/TypeScript/issues/28884#issuecomment-448356158
       const props = { ...this.props, ...boundProps } as T;
 
-      // https://github.com/Microsoft/TypeScript/issues/28884#issuecomment-448356158
       return <C {...props} />;
     }
   };
