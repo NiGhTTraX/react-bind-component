2d8cb2c37b23c33cea5730e18148a1289ac1029d
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-09-18 21:00:01 +0200

    Update docstring

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,12 @@ export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 
 /**
  * Create a HOC that binds some props for the wrapped component.
+ *
+ * @param C The component that will be wrapped.
+ * @param boundProps A subset of the component's props.
+ *
+ * @returns A new component that accepts all of the original
+ *   component's props minus the ones that were bound.
  */
 export default function bindComponent<T, K extends keyof T>(
   C: ComponentType<T>,
